27297710a38297c367d236687b95d8d13303d850
Chaxob <74979039+Chaxob@users.noreply.github.com>
2026-01-08 11:39:25 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -27,12 +27,14 @@ def meteo():
         temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
+  
 @app.route("/rapport/")
 def mongraphique():
-    return render_template("graphique.html") 
+    return render_template("graphique.html")
+  
 @app.route("/histogramme/")
-def mongraphique():
-    return render_template("histogramme.html")  
+def tawaranographique():
+    return render_template("histogramme.html")
   
   
 if __name__ == "__main__":
